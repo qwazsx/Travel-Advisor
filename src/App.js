f62e432347d73fb2d422e258f4f660879377ebc5
FATIH CAMBEK <fatih_cambek@hotmail.com>
2022-02-06 21:00:00 +0300

    TravelAdvisor app initialize

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//imr import React from 'react';
+//imrd import ReactDOM from 'react-dom';
+
+import React from 'react';
+
+const App = () => {
+
+    return(
+        <div>
+            <h1></h1>
+        </div>
+    )
+}
+
