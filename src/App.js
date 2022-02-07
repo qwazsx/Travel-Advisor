161a3e19144658608ef877d6d76f7158f14714a5
FATIH CAMBEK <fatih_cambek@hotmail.com>
2022-02-07 22:34:36 +0300

    Header kısmı eklendi

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 //imr import React from 'react';
 //imrd import ReactDOM from 'react-dom';
-//imrd import ReactDOM from 'react-dom';
+//17. dakikada kaldım
 
 import React from 'react';
+import { CssBaseline,Grid } from '@material-ui/core'; //CssBaseline padding margin, bgcolor düzenleyip normale çekiyor
+
+
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
 
 const App = () => {
 
     return(
-        <div>
-            <h1>Hello, World!</h1>
-
-        </div>
+        <>
+            <CssBaseline/>
+            <Header/>
+            <Grid container spacing={3} style={{ width: '100%'}}> 
+            {/* style içine obje aldığı için width'i böyle çift parantezle verdik */}
+                {/* xs=12 ile mobil cihazlarda full ekran görüntülenecek demiş olduk */}
+                {/* md=4 ile daha büyük cihazlarda görüntülenecek alanı belirttik */}
+                <Grid item xs={12} md={4}>
+                    <List/>
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map/>
+                </Grid>
+            </Grid>
+        </>
     );
 }
 
