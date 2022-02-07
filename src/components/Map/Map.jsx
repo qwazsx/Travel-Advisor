f15ea39c07c4d63d003655342ab288d3fd340c1e
FATIH CAMBEK <fatih_cambek@hotmail.com>
2022-02-08 00:00:41 +0300

    Google maps Api Key ile harita eklemesi yapıldı.

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+// useMediaQuery mobile responsive yapmamıza yarayacak
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
+import Rating from '@material-ui/lab';
+// bazı meterial-ui importları hala geliştirilmekte olduğundan(Rating gibi) /core'dan değil /lab'den aldık
 
+import useStyles from './styles';
 
 const Map = () => {
-    return(
-        <h1>Map</h1>
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+
+    const coordinates = { lat: 0, lng: 0 };
+
+    return (
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyCpJBNkbTcv-qFNGSFeKXz_NFiEN5ZiLi4' }}
+                defaultCenter={ coordinates }
+                center={ coordinates }
+                defaultZoom={14}
+                margin={[50,50,50,50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+                >
+
+            </GoogleMapReact>
+
+        </div>
     );
 }
 
