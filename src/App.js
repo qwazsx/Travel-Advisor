56667e032872c426e96baf055838207e320832fa
FATIH CAMBEK <fatih_cambek@hotmail.com>
2022-02-08 19:58:32 +0300

    ListInBoundary endpointi ile restoranları aldık

--- src/App.js
+++ src/App.js
@@ -3,29 +3,54 @@
 //17. dakikada kaldım
 //37.58de kaldım
 
-import React from 'react';
-import { CssBaseline,Grid } from '@material-ui/core'; //CssBaseline padding margin, bgcolor düzenleyip normale çekiyor
+import React, { useState, useEffect } from 'react';
+import { CssBaseline, Grid } from '@material-ui/core'; //CssBaseline padding margin, bgcolor düzenleyip normale çekiyor
 
-
+import { getPlacesData } from './api';
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
 
 const App = () => {
 
-    return(
+    const [places, setPlaces] = useState([]);
+
+    const [coordinates, setCoordinates] = useState({});
+    const [bounds, setBounds] = useState(null);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+            setCoordinates({ lat: latitude, lng: longitude });
+        })
+    }, []);// boş array olunca sadece ilk yüklemede çalışıyor. ilk yüklediğimizde kendi lokasyonumuzu alsın diye kullandık
+
+    useEffect(() => {
+        // console.log(coordinates,bounds); bununla başta görebilcek mi kendi lokasyonumuzu diye baktık
+
+        getPlacesData(bounds.sw, bounds.ne)
+            .then((data) => {
+                console.log(data);
+                setPlaces(data);
+            })
+    }, [coordinates, bounds]);
+
+    return (
         <>
-            <CssBaseline/>
-            <Header/>
-            <Grid container spacing={3} style={{ width: '100%'}}> 
-            {/* style içine obje aldığı için width'i böyle çift parantezle verdik */}
+            <CssBaseline />
+            <Header />
+            <Grid container spacing={3} style={{ width: '100%' }}>
+                {/* style içine obje aldığı için width'i böyle çift parantezle verdik */}
                 {/* xs=12 ile mobil cihazlarda full ekran görüntülenecek demiş olduk */}
                 {/* md=4 ile daha büyük cihazlarda görüntülenecek alanı belirttik */}
                 <Grid item xs={12} md={4}>
-                    <List/>
+                    <List />
                 </Grid>
                 <Grid item xs={12} md={8}>
-                    <Map/>
+                    <Map
+                        setCoordinates={setCoordinates}
+                        setBounds={setBounds}
+                        coordinates={coordinates}
+                    />
                 </Grid>
             </Grid>
         </>
---
+++ src/api/index.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+//api calları için bu js'i oluşturduk
+// axios ile api call yapacağız
+
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+
+export const getPlacesData = async (sw,ne) => {
+    try {
+        // axios olduğundan metod get olur dedi ve yukarıdaki parametrelerden get'i çıkardık
+        const {data: {data}} = await axios.get(URL, {
+            params: {
+              bl_latitude: sw.lat,
+              tr_latitude: ne.lat,
+              bl_longitude: sw.lng,
+              tr_longitude: ne.lng,
+            },
+            headers: {
+              'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+              'x-rapidapi-key': '71bd949779mshdef02ead95339a8p14dc1cjsn41ceb8e4ca82'
+            }
+          });
+
+        return data;
+    } catch (error) {
+        console.log(error);
+    }     
+}
